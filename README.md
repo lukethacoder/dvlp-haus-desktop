c515310671e0dd9b1791336ecbc94b77aaafe62c
luke-secomb <lukessecomb@gmail.com>
2020-01-03 12:46:00 +1100

    come electron and webpack config

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# dvlp-haus-desktop
-desktop app + design refresh of dvlp.haus
+# dvlp.haus desktop
+
+> design refresh of dvlp.haus + desktop rebuild
+
+### Tech stack
+
+- [Electron](https://electronjs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [React](https://reactjs.org/)
+- [React Router (v5)](https://reacttraining.com/react-router/web)
+- [Webpack](https://webpack.js.org/)
+- [Jest](https://facebook.github.io/jest/)
+- [SASS/SCSS](https://sass-lang.com/)
+
+## Install
+
+Clone the repository
+
+Install the dependencies:
+
+```bash
+yarn
+```
+
+## Usage
+
+```bash
+yarn dev
+```
+
+## Packaging
+
+We use [Electron Forge](https://www.electronforge.io/) to build and package the application. By default you can run the following to package for your current platform:
+
+```bash
+yarn package
+```
+
+This will create an unzipped version of your application in the `out` folder.
+
+```bash
+yarn make
+```
+
+This will create a distributable version of your app.
---
+++ webpack.main.config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  /**
+   * This is the main entry point for your application, it's the first file
+   * that runs in the main process.
+   */
+  entry: './src/main/main.ts',
+  // // Put your normal webpack config below here
+  module: {
+    rules: [
+      // Add support for native node modules
+      {
+        test: /\.node$/,
+        use: 'node-loader',
+      },
+      {
+        test: /\.(m?js|node)$/,
+        exclude: /(.webpack|node_modules)/,
+        parser: { amd: false },
+        use: {
+          loader: '@marshallofsound/webpack-asset-relocator-loader',
+          options: {
+            outputAssetBase: 'native_modules',
+          },
+        },
+      },
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true,
+          babelrc: false,
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                useBuiltIns: 'usage',
+                corejs: 3,
+                targets: { browsers: 'last 2 versions ' },
+              },
+            ],
+            '@babel/preset-typescript',
+            '@babel/preset-react',
+          ],
+          plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
+        },
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+    ],
+  },
+}
