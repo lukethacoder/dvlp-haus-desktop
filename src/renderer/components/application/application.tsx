e43a6d4de9cff2f5b9dfaf9222c7c1b4f62b6e2f
luke-secomb <lukessecomb@gmail.com>
2020-01-06 08:26:13 +1100

    add color mapping / add light + dark toggle button (for dev - will be in settings cog in prod)

--- src/renderer/components/application/application.tsx
+++ src/renderer/components/application/application.tsx
@@ -5,15 +5,17 @@ import { Resizable } from 're-resizable'
 
 const Application = () => {
   const [sideNavWidth, setSideNavWidth] = useState(196)
+  const [themeColor, setThemeColor] = useState('dark')
 
   return (
-    <div className='application__container'>
+    <div className={`application__container ${themeColor}`}>
       <Resizable
         size={{ width: sideNavWidth, height: 'auto' }}
         onResizeStop={(e: any, direction: any, ref: any, d: any) => {
           setSideNavWidth(sideNavWidth + d.width)
         }}
       >
+        <button onClick={() => setThemeColor(themeColor === 'dark' ? 'light' : 'dark')}>toggle theme</button>
         <NavigationSide />
       </Resizable>
       <section className='application__section'>
