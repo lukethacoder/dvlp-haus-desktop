5aa3916e2486bed26127a3e7aae62396f4ac3945
Luke Secomb <luke.secomb@live.com>
2020-05-07 21:26:08 +1000

    ⛏️ add rust feat. very basic example

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 // import Header from './components/header'
 // import RustExample from './components/rust-example'
+import { add } from '../rust/lib.rs'
 
 class App extends Component {
+  calcFromRust() {
+    console.log('add ', add(3, 7))
+  }
+
   render() {
     return (
       <div>
         <h1>dvlp.haus</h1>
+        <button onClick={this.calcFromRust}>calc rust</button>
         {/* <Header />
         <RustExample /> */}
       </div>
