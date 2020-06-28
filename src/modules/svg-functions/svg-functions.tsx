86e92a04213567c9eeb7a2bd7435c326cf23c147
Luke Secomb <luke.secomb@live.com>
2020-06-28 20:46:09 +1000

    🛅 polish svg converter module / minor global css (textareas)

--- src/modules/svg-functions/svg-functions.tsx
+++ src/modules/svg-functions/svg-functions.tsx
@@ -1,10 +1,15 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState, useContext } from 'react'
 import Clipboard from 'react-clipboard.js'
+import NotyfContext from '~/src/context/notify'
 
 import { ModuleWrapper } from '~/src/components/module-wrapper'
 import { encodeSVG, checkIfValidSVG, invertQuoteType } from './utils'
 
+import CopySVG from '~/src/assets/icons/fa/copy.svg'
+
 const SvgFunctions: FC = () => {
+  const notyf = useContext(NotyfContext)
+
   const [svgInput, setSvgInput] = useState('')
   const [pureDataOutput, setPureDataOutput] = useState('')
   const [cssBackgroundOutput, setCssBackgroundOutput] = useState('')
@@ -18,10 +23,7 @@ const SvgFunctions: FC = () => {
           `"data:image/svg+xml,${invertQuoteType(encodeSVG(event.target.value), 'single')}"`
         )
         setCssBackgroundOutput(
-          `background-image: url("data:image/svg+xml,${invertQuoteType(
-            encodeSVG(event.target.value),
-            'single'
-          )}");`
+          `url("data:image/svg+xml,${invertQuoteType(encodeSVG(event.target.value), 'single')}")`
         )
         setSvgPreview(event.target.value)
       } else {
@@ -40,81 +42,120 @@ const SvgFunctions: FC = () => {
 
   return (
     <ModuleWrapper>
-      <div>
-        <h3>Svg to base64</h3>
-
-        <div className='w-full flex flex-wrap'>
-          <div className='w-full md:w-3/5 order-2 md:order-1 md:pr-2'>
-            <div className='w-full flex flex-wrap pb-2'>
-              <div className='w-full flex flex-col relative'>
-                <Clipboard className='absolute bottom-0 right-0' data-clipboard-text={svgInput}>
-                  Copy
-                </Clipboard>
-                <label className='w-full'>SVG Code</label>
-                <textarea
-                  value={svgInput}
-                  name='svgCode'
-                  placeholder='paste your <svg>here</svg>'
-                  spellCheck={false}
-                  onChange={handleInputChange}
-                  className='w-full'
-                ></textarea>
-              </div>
-            </div>
-            <div className='w-full flex flex-wrap pb-2'>
-              <div className='w-full flex flex-col relative'>
+      <div className='w-full flex flex-wrap'>
+        <div className='w-full md:w-3/5 order-2 md:order-1 md:pr-2'>
+          <div className='w-full flex flex-wrap pb-2'>
+            <div className='w-full flex flex-col relative'>
+              <div className='textarea_actions'>
                 <Clipboard
-                  className='absolute bottom-0 right-0'
-                  data-clipboard-text={pureDataOutput}
+                  className=''
+                  data-clipboard-text={svgInput}
+                  onSuccess={() => notyf.success('Copied output')}
                 >
-                  Copy
+                  <div
+                    className={`icon w-4 h-4 bg-font hover:bg-brand cursor-pointer`}
+                    style={{
+                      maskImage: `url(${CopySVG})`,
+                      WebkitMaskImage: `url(${CopySVG})`,
+                      paddingBottom: '100%',
+                    }}
+                  ></div>
                 </Clipboard>
-                <label className='w-full'>Pure Data Output</label>
-                <textarea
-                  value={pureDataOutput}
-                  name='pureDataOutput'
-                  spellCheck={false}
-                  readOnly={true}
-                  onChange={handleInputChange}
-                  className='w-full'
-                ></textarea>
               </div>
-            </div>
-            <div className='w-full flex flex-wrap pb-2'>
-              <div className='w-full flex flex-col relative'>
-                <Clipboard
-                  className='absolute bottom-0 right-0'
-                  data-clipboard-text={cssBackgroundOutput}
-                >
-                  Copy
-                </Clipboard>
-                <label className='w-full'>CSS Background Image</label>
-                <textarea
-                  value={cssBackgroundOutput}
-                  name='cssBackgroundImage'
-                  spellCheck={false}
-                  readOnly={true}
-                  onChange={handleInputChange}
-                  className='w-full'
-                ></textarea>
-              </div>
+              <label className='w-full pb-1'>SVG Source Code</label>
+              <textarea
+                value={svgInput}
+                name='svgCode'
+                placeholder='paste your <svg>here</svg>'
+                spellCheck={false}
+                onChange={handleInputChange}
+                rows={12}
+                className='w-full'
+              ></textarea>
             </div>
           </div>
-          <div className='w-full md:w-2/5 order-1 md:order-2 md:pl-2 py-2 md:pt-0'>
-            <div className='border-color border-solid border p-4'>
-              <div className=''>
-                {svgPreview ? (
-                  svgPreview.includes('<svg') && svgPreview.includes('</svg>') ? (
-                    <div
-                      className=''
-                      dangerouslySetInnerHTML={{
-                        __html: `${svgPreview}`,
-                      }}
-                    ></div>
-                  ) : null
-                ) : null}
-                <div className='text-font opacity-20'>svg preview</div>
+          <div className='w-full flex flex-wrap pb-2'>
+            <div className='w-full flex flex-col relative'>
+              <div className='textarea_actions'>
+                <Clipboard
+                  className=''
+                  data-clipboard-text={pureDataOutput}
+                  onSuccess={() => notyf.success('Copied output')}
+                >
+                  <div
+                    className={`icon w-4 h-4 bg-font hover:bg-brand cursor-pointer`}
+                    style={{
+                      maskImage: `url(${CopySVG})`,
+                      WebkitMaskImage: `url(${CopySVG})`,
+                      paddingBottom: '100%',
+                    }}
+                  ></div>
+                </Clipboard>
               </div>
+              <label className='w-full pb-1'>base64 Output</label>
+              <textarea
+                value={pureDataOutput}
+                name='pureDataOutput'
+                spellCheck={false}
+                readOnly={true}
+                onChange={handleInputChange}
+                rows={6}
+                className='w-full'
+              ></textarea>
+            </div>
+          </div>
+          <div className='w-full flex flex-wrap pb-2'>
+            <div className='w-full flex flex-col relative'>
+              <div className='textarea_actions'>
+                <Clipboard
+                  className=''
+                  data-clipboard-text={cssBackgroundOutput}
+                  onSuccess={() => notyf.success('Copied output')}
+                >
+                  <div
+                    className={`icon w-4 h-4 bg-font hover:bg-brand cursor-pointer`}
+                    style={{
+                      maskImage: `url(${CopySVG})`,
+                      WebkitMaskImage: `url(${CopySVG})`,
+                      paddingBottom: '100%',
+                    }}
+                  ></div>
+                </Clipboard>
+              </div>
+              <label className='w-full pb-1'>CSS url()</label>
+              <textarea
+                value={cssBackgroundOutput}
+                name='cssBackgroundImage'
+                spellCheck={false}
+                readOnly={true}
+                onChange={handleInputChange}
+                rows={6}
+                className='w-full'
+              ></textarea>
+            </div>
+          </div>
+        </div>
+        <div className='w-full md:w-2/5 order-1 md:order-2 md:pl-2 py-2 md:pt-0'>
+          <div className='border-color border-solid border p-4'>
+            <div className=''>
+              {svgPreview ? (
+                svgPreview.includes('<svg') && svgPreview.includes('</svg>') ? (
+                  <div
+                    className=''
+                    dangerouslySetInnerHTML={{
+                      __html: `${svgPreview}`,
+                    }}
+                  ></div>
+                ) : (
+                  <div className='text-font opacity-20 flex items-center justify-center'>
+                    <p>invalid svg</p>
+                  </div>
+                )
+              ) : (
+                <div className='text-font opacity-20 flex items-center justify-center'>
+                  <p>svg preview</p>
+                </div>
+              )}
             </div>
           </div>
         </div>
